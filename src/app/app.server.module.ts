ee90f6c6b4dffce829d38ba178034e85c1b7d411
shakti97 <shaktisingh230797@gmail.com>
2018-10-20 20:34:06 +0530

    Added the BaseHref in the server module

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,6 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   imports: [
@@ -11,7 +12,10 @@ import { AppComponent } from './app.component';
     ModuleMapLoaderModule
   ],
   providers: [
-   
+    {
+      provide: APP_BASE_HREF,
+      useValue: 'http://jsonplaceholder.typicode.com'
+    }
   ],
   bootstrap: [ AppComponent ],
 })
